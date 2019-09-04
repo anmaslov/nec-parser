4e5c4193b5ddd89656f19405a56173f718b858e4
Aleksey Maslov <lelick29@gmail.com>
2019-09-04 21:03:27 +0300

    #5 change logger to zap

--- kpi/kpi.go
+++ kpi/kpi.go
@@ -7,13 +7,14 @@ const (
 	startKpi float32 = 1   //start value
 )
 
+// kpi структура коеффициентов
 type kpi struct {
 	min     float32
 	max     float32
 	current float32
 }
 
-//NewKpi создает новый коэффициент приращения
+// NewKpi создает новый коэффициент приращения
 func NewKpi() *kpi {
 	return &kpi{
 		min:     minKpi,
@@ -22,11 +23,12 @@ func NewKpi() *kpi {
 	}
 }
 
+// GetCurrent получить текущее значение
 func (k *kpi) GetCurrent() float32 {
 	return k.current
 }
 
-//StepDown уменьшить коэффициент
+// StepDown уменьшить коэффициент
 func (k *kpi) StepDown() {
 	if k.current /= 2; k.current < minKpi {
 		k.current = minKpi
@@ -40,12 +42,14 @@ func (k *kpi) StepUp() {
 	}
 }
 
+// ForceDown быстро уменьшить коэффициент
 func (k *kpi) ForceDown() {
 	if k.current /= 8; k.current < minKpi {
 		k.current = minKpi
 	}
 }
 
+// ForceUp быстро увеличить коэффициент
 func (k *kpi) ForceUp() {
 	if k.current *= 2 * 2 * 2; k.current > maxKpi {
 		k.current = maxKpi
--- main.go
+++ main.go
@@ -3,101 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/anmaslov/nec-parser/config"
-	"github.com/anmaslov/nec-parser/kpi"
-	"github.com/anmaslov/smdr"
 	"github.com/jessevdk/go-flags"
-	"log"
-	"net"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"os"
-	"strings"
-	"time"
 )
 
 var cfg config.Config
-
-type DataProducer struct {
-	OutChan chan CallInfo
-}
-
-func (p *DataProducer) getOutChan() <-chan CallInfo {
-	return p.OutChan
-}
-
-const MAX_PARSED_CICLES int = 100
-
-//Получение данных со станции
-func stantionListener(phone Phones, p DataProducer) {
-	for {
-		addr := strings.Join([]string{phone.Ip, phone.Port}, ":")
-		stDesc := string(phone.Id)
-		conn, err := net.Dial("tcp", addr)
-		if err != nil {
-			log.Println("dial error on addr:", addr, err)
-			time.Sleep(time.Minute * 5)
-			continue
-		}
-		k := kpi.NewKpi()
-		//Основной цикл для получения данных
-		i := 0 //Счетчик распарсенных данных
-		for {
-			r1 := smdr.SetRequest(smdr.DataRequest())
-			if wr, err := conn.Write([]byte(r1)); //Запрос #1
-			wr == 0 || err != nil {
-				log.Println(addr, err)
-				break
-			}
-
-			err = conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-			if err != nil {
-				log.Println(addr, err)
-				break
-			}
-
-			log.Println("trying to get response from", stDesc)
-			buff := make([]byte, 1024)
-			rd, err := conn.Read(buff)
-			if err != nil {
-				log.Println(addr, err)
-			}
-
-			log.Println("trying to parse data", stDesc)
-			res := smdr.CDR{}
-			err = res.Parser(buff[:rd])
-			if err != nil {
-				log.Println(err, stDesc)
-				k.StepUp()
-			} else {
-				call := fillParam(&res)
-				call.Stantion = string(phone.Id)
-				p.OutChan <- call // Отправляем данные в канал
-				//Отправляем запрос о том, что все ок
-				r4 := smdr.SetRequest(smdr.ClientResponse(res.Sequence))
-				if wr, err := conn.Write([]byte(r4)); //Запрос #4
-				wr == 0 || err != nil {
-					log.Println(err, stDesc)
-				} else {
-					k.StepDown() //Уменьшаем интервал
-					i++          //Увеличиваем счетчик распарсеных данных
-				}
-			}
-
-			if i >= MAX_PARSED_CICLES {
-				log.Println("disconnect from", stDesc, "limit of parsed data", i)
-				break
-			}
-
-			d := time.Duration(k.GetCurrent() * float32(time.Second))
-			log.Println("sleep on", d, stDesc)
-
-			time.Sleep(d)
-		} //end for
-
-		conn.Close()
-
-		log.Println("error, when connect or receive data", stDesc, "wait 60seconds")
-		time.Sleep(time.Minute) //Ждем 1 минуту, прежде чем выполнить повторное подключение
-	}
-}
 
 func init() {
 	parser := flags.NewParser(&cfg, flags.Default)
@@ -109,15 +21,31 @@ func init() {
 	}
 }
 
-func main() {
-	//todo переделать логгер
-	f, err := os.OpenFile("/var/log/phone.log", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+// initLogger создает и настривает новый экземпляр логера
+func initLogger() (*zap.Logger, error) {
+	lvl := zap.InfoLevel
+	err := lvl.UnmarshalText([]byte(cfg.LogLevel))
 	if err != nil {
-		fmt.Println("file no exist", err)
+		return nil, fmt.Errorf("can't unmarshal log-level: %s", err)
 	}
-	defer f.Close()
-	log.SetOutput(f)
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	opts := zap.NewProductionConfig()
+	opts.Level = zap.NewAtomicLevelAt(lvl)
+	opts.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	if !cfg.LogJSON {
+		opts.Encoding = "console"
+		opts.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+
+	return opts.Build()
+}
+
+func main() {
+	logger, err := initLogger()
+	if err != nil {
+		fmt.Printf("error on init logger: %s.\nFor help use -h\n", err.Error())
+		os.Exit(1)
+	}
 
 	session := initialiseMongo()
 	mongoStore.session = session
@@ -130,20 +58,20 @@ func main() {
 
 	phones, err := getPhones()
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("unable to get phones", zap.Error(err))
 	}
+
 	for _, phone := range phones {
-		go stantionListener(phone, p) //Запускаем столько гоурутин, сколько телефонов
+		go stListener(phone, p, logger)
 	}
 
 	for data := range p.getOutChan() { //Ждем данных от канала
-		//fmt.Println("get data from chain", data)
 		err := insertCall(&data)
 		if err != nil {
-			log.Fatal(err) //Падаем, т.к. запись в базу - критична
-		} else {
-			log.Println("write to DB success, date end call:", data.Cvt.DateEnd.String())
+			logger.Fatal("unable to write db", zap.Error(err)) //Падаем, т.к. запись в базу - критична
 		}
+
+		logger.Debug("write to DB success, date end call", zap.String("date_end", data.Cvt.DateEnd.String()))
 	}
 
 }
